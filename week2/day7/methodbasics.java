e1b4efb19f456d6985b1d0f1481ea82533035633
saiprasanna04 <guttasaiprasanna7485@gmail.com>
2026-07-14 18:43:04 +0530

    day-7

---
+++ week2/day7/methodbasics.java
@@ -0,0 +1,8 @@
+public class methodbasics{
+    static void greet(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        greet();
+    }
+}
---
+++ week2/day7/scope.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // Code here CANNOT use x
+
+    int x = 100;
+
+    // Code here CAN use x
+    System.out.println(x);
+  }
+}
